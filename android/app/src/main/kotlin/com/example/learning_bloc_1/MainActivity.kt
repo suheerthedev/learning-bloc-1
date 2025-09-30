14dadd4e38361be505de3e111296cf66e96098a9
suheerthedev <khansuheer1@gmail.com>
2025-09-30 17:40:21 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/learning_bloc_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learning_bloc_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,23 @@
+final List<Map<String, dynamic>> groceryItems = [
+  {
+    'id': 1,
+    "name": "Apple",
+    "description": "Fresh red apple, crisp and sweet",
+    "imageUrl":
+        "https://images.unsplash.com/photo-1560806887-1e4cd0b6cbd6?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // example from Unsplash
+  },
+  {
+    'id': 2,
+    "name": "Banana",
+    "description": "Ripe yellow banana, rich in potassium",
+    "imageUrl":
+        "https://images.unsplash.com/photo-1587132137056-bfbf0166836e?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    'id': 3,
+    "name": "Tomato",
+    "description": "Juicy red tomato, great for salads",
+    "imageUrl":
+        "https://unsplash.com/photos/o9xS0fjS4Gw/download?force=true&w=640",
+  },
+];
