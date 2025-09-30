47bf558d3fc0532c867ea1a773b8d157312fdef9
suheerthedev <khansuheer1@gmail.com>
2025-09-30 18:26:26 +0500

    Wrote all the possible events

--- lib/features/home/bloc/home_event.dart
+++ lib/features/home/bloc/home_event.dart
@@ -2,3 +2,11 @@ part of 'home_bloc.dart';
 
 @immutable
 sealed class HomeEvent {}
+
+class HomeProductCartButtonPressed extends HomeEvent {}
+
+class HomeProductWishlistButtonPressed extends HomeEvent {}
+
+class HomeCartButtonPressed extends HomeEvent {}
+
+class HomeWishlistButtonPressed extends HomeEvent {}
