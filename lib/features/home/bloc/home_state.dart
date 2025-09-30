09520ea2a28258edec6c4454f9ee56b9c0c47936
suheerthedev <khansuheer1@gmail.com>
2025-09-30 19:14:33 +0500

    Implemented States

--- lib/features/home/bloc/home_state.dart
+++ lib/features/home/bloc/home_state.dart
@@ -3,4 +3,18 @@ part of 'home_bloc.dart';
 @immutable
 sealed class HomeState {}
 
+sealed class HomeActionState extends HomeState {}
+
 final class HomeInitial extends HomeState {}
+
+final class HomeLoadingState extends HomeState {}
+
+final class HomeLoadSuccessState extends HomeState {}
+
+final class HomeErrorState extends HomeState {}
+
+//Action State
+
+final class HomeNavigateToWishlistPageActionState extends HomeActionState {}
+
+final class HomeNavigateToCartPageActionState extends HomeActionState {}
