812691c29a8d469efa6dba96081f787e3ed731f0
suheerthedev <khansuheer1@gmail.com>
2025-10-02 18:49:20 +0500

    Added Event trigger in HomeBloc

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 part 'home_event.dart';
@@ -6,8 +9,23 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<HomeCartButtonPressed>(_onNavigateCart);
+    on<HomeWishlistButtonPressed>(_onNavigateWishlist);
+  }
+
+  FutureOr<void> _onNavigateCart(
+    HomeCartButtonPressed event,
+    Emitter<HomeState> emit,
+  ) {
+    debugPrint("onNavigate to Cart called!!!");
+    emit(HomeNavigateToCartPageActionState());
+  }
+
+  FutureOr<void> _onNavigateWishlist(
+    HomeWishlistButtonPressed event,
+    Emitter<HomeState> emit,
+  ) {
+    debugPrint("onNavigate to Wishlist called!!!");
+    emit(HomeNavigateToWishlistPageActionState());
   }
 }
