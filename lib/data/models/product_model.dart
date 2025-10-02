15506f6ba86ab548d7d9ffb6a91452c7d13630c0
suheerthedev <khansuheer1@gmail.com>
2025-10-02 19:25:47 +0500

    Created ProductModel

---
+++ lib/data/models/product_model.dart
@@ -0,0 +1,35 @@
+class ProductModel {
+  final int id;
+  final String name;
+  final String description;
+  final num price;
+  final String imageUrl;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      price: json['price'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'price': price,
+      'imageUrl': imageUrl,
+    };
+  }
+}
