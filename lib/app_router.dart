f45a62767a3c13826a8bb855064ba5eb9a00c2e5
suheerthedev <khansuheer1@gmail.com>
2025-10-02 18:57:27 +0500

    Setted up app_router.dart

---
+++ lib/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:learning_bloc_1/features/cart/view/cart_page.dart';
+import 'package:learning_bloc_1/features/home/view/home_page.dart';
+import 'package:learning_bloc_1/features/wishlist/view/wishlist_page.dart';
+
+class AppRouter {
+  dynamic onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => Home());
+      case '/cart':
+        return MaterialPageRoute(builder: (_) => CartPage());
+      case '/wishlist':
+        return MaterialPageRoute(builder: (_) => WishlistPage());
+      default:
+        return null;
+    }
+  }
+}
